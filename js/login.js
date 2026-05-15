e187dc749af532c09de2052e5ba666ffe2f8049e
Oswaldo <dazaoswaldo2@gmail.com>
2026-05-15 17:34:32 -0500

    feat/agregados-formularios

---
+++ js/login.js
@@ -0,0 +1,8 @@
+const btn_text_registrarse = document.getElementById("btn_text")
+const registro_contenedor = document.getElementById("sign_up_box")
+const login_contenedor = document.getElementById("login_box")
+
+btn_text_registrarse.addEventListener("click",()=>{
+    registro_contenedor.classList.remove("invisible")
+    login_contenedor.classList.add("invisible")
+})
