3e3fa1813e090c7b9f74fea7322c9ef124053f59
Oswaldo <dazaoswaldo2@gmail.com>
2026-05-16 09:45:15 -0500

    ningun-cambio-solo-quiero-guardar

--- php/crear_sorteo.php
+++ php/crear_sorteo.php
@@ -10,11 +10,26 @@ $ahora->setTime($ahora->format('H'), 0, 0);
 $hora_proxima = $ahora->format("Y-m-d H:00:00");
 $fecha = date("Y-m-d");
 
-$sql = "SELECT * FROM sorteos WHERE fecha = '$fecha' AND hora_revelacion = '$hora_proxima'";
+$sql_validar_sorteo_activo = "SELECT * FROM sorteos WHERE esta_activo=1 LIMIT 1";
+$resultado_sorteo_activo= mysqli_query($conexion,$sql_validar_sorteo_activo)
 
-$resultado = mysqli_query($conexion, $sql);
+if(mysqli_num_rows($resultado_sorteo_activo)>0){
+    $sorteo_activo=mysqli_fetch_assoc($resultado_sorteo_activo);
+    
+    if($sorteo_activo["fecha"]<$fecha || ($sorteo_activo["fecha"]==$fecha && $sorteo_activo["hora_revelacion"]<$ahora)){
+        
+        $id = $sorteo_activo["id"];
+        $sql_cerrar_sorteo = "UPDATE sorteos SET esta_activo = 0 WHERE id='$id'";
+        $resultado_updtae=mysqli_query($conexion,$sql_cerrar_sorteo);
+        
+    }else{
 
-if (mysqli_num_rows($resultado)==0){
-    mysqli_query($conexion, "INSERT INTO sorteos (fecha, hora_revelacion, esta_activo)VALUES ('$fecha', '$hora_proxima', 1)");
+        die;
+
+    }
+
 }
+
+mysqli_query($conexion, "INSERT INTO sorteos (fecha, hora_revelacion, esta_activo)VALUES ('$fecha', '$hora_proxima', 1)");
+
 ?>
